8d5645a812e4765d02642cb562c32fb7b90a5b6e
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-03-17 14:12:24 +0530

    PWA conversion 👍

--- next.config.js
+++ next.config.js
@@ -1,10 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     domains: ["lh3.googleusercontent.com"],
+//   },
+// }
+
+// module.exports = nextConfig
+
+
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
   reactStrictMode: true,
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
-}
-
-module.exports = nextConfig
-
+});
