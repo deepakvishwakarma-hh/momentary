7081847e60cec95930535009e29f607f59221921
Deepak Vishwakarma <79501955+deepakvishwakarma-hh@users.noreply.github.com>
2022-03-18 09:29:59 +0530

    update : comment out Validation 🚀

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -67,176 +67,176 @@ const Index = () => {
                 <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' />
             </Head>
 
-            {/* <Validator> */}
+            <Validator>
 
-            <Head>
-                <title>Momentary</title>
-                <meta name="theme-color" content="#16161D" />
-                <link rel="shortcut icon" href="/transparent.png" />
-                <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'
-                />
-            </Head>
+                <Head>
+                    <title>Momentary</title>
+                    <meta name="theme-color" content="#16161D" />
+                    <link rel="shortcut icon" href="/transparent.png" />
+                    <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'
+                    />
+                </Head>
 
-            <RoomAleart />
+                <RoomAleart />
 
-            <Grid
-                position={'fixed'}
-                templateRows={"150px auto"}
-                templateColumns={'100%'}
-                width={'100%'}
-                height={'100%'}
-                bg={'blackAlpha.900'}>
+                <Grid
+                    position={'fixed'}
+                    templateRows={"150px auto"}
+                    templateColumns={'100%'}
+                    width={'100%'}
+                    height={'100%'}
+                    bg={'blackAlpha.900'}>
 
-                <Flex px={[0, 10]}
-                    justifyContent={"space-between"}
-                    bg="none"
-                    alignItems={'center'}>
-
-                    <Flex pl={3}
+                    <Flex px={[0, 10]}
+                        justifyContent={"space-between"}
+                        bg="none"
                         alignItems={'center'}>
 
-                        <Image src="/transparent.png"
-                            width={40}
-                            height={40}
-                            alt="icons" />
-                        <Text
-                            color="white"
-                            px={3}
-                            fontSize="20">Momentary</Text>
-                    </Flex>
-                    <Tooltip label="Logged in with Google security, click to logout">
-                        <Flex
-                            onClick={logoutHandler}
-                            border={"2px rgba(255, 255, 255, 0.116) solid"}
-                            borderRadius={50}
-                            py={1}
-                            px={3}
-                        >
-                            <Image
-                                src="/google.svg"
-                                width={30}
-                                height={30}
-                                alt="icons"
-                            />
+                        <Flex pl={3}
+                            alignItems={'center'}>
+
+                            <Image src="/transparent.png"
+                                width={40}
+                                height={40}
+                                alt="icons" />
                             <Text
-                                display={['none', 'block']}
-                                p={2}
-                                textTransform={'capitalize'}
-                                color="white">{user?.displayName}</Text>
+                                color="white"
+                                px={3}
+                                fontSize="20">Momentary</Text>
                         </Flex>
-                    </Tooltip>
-                </Flex>
-                <Flex
-                    bgImage={["/h.svg", "/h2.svg"]}
-                    bgRepeat={'no-repeat'}
-                    bgSize="cover"
-                    flexDirection={['column', 'column', 'column', 'row']}
-                    alignItems={"center"}
-                    justifyContent={"space-around"}
-                    pt={10}
-                    borderRadius={"3rem 3rem 0 0"}>
+                        <Tooltip label="Logged in with Google security, click to logout">
+                            <Flex
+                                onClick={logoutHandler}
+                                border={"2px rgba(255, 255, 255, 0.116) solid"}
+                                borderRadius={50}
+                                py={1}
+                                px={3}
+                            >
+                                <Image
+                                    src="/google.svg"
+                                    width={30}
+                                    height={30}
+                                    alt="icons"
+                                />
+                                <Text
+                                    display={['none', 'block']}
+                                    p={2}
+                                    textTransform={'capitalize'}
+                                    color="white">{user?.displayName}</Text>
+                            </Flex>
+                        </Tooltip>
+                    </Flex>
                     <Flex
-                        px={5}
-                        flexDir={'column'}>
-                        <Text
-                            py={[5, 0]}
-                            fontFamily={"Fredoka One"}
-                            fontSize={['4vh', 50, 50, 70]}
-                            color="white">Feel Free & Create <br />
+                        bgImage={["/h.svg", "/h2.svg"]}
+                        bgRepeat={'no-repeat'}
+                        bgSize="cover"
+                        flexDirection={['column', 'column', 'column', 'row']}
+                        alignItems={"center"}
+                        justifyContent={"space-around"}
+                        pt={10}
+                        borderRadius={"3rem 3rem 0 0"}>
+                        <Flex
+                            px={5}
+                            flexDir={'column'}>
                             <Text
-                                display="inline"
-                                color="tomato">Secret</Text> chat with <br />
-                            Momentary
-                        </Text>
-                        <Text color="white"
-                            onClick={() => { router.push('https://github.com/deepakvishwakarma-hh/momentary-talk#readme') }}>Read more
+                                py={[5, 0]}
+                                fontFamily={"Fredoka One"}
+                                fontSize={['4vh', 50, 50, 70]}
+                                color="white">Feel Free & Create <br />
+                                <Text
+                                    display="inline"
+                                    color="tomato">Secret</Text> chat with <br />
+                                Momentary
+                            </Text>
+                            <Text color="white"
+                                onClick={() => { router.push('https://github.com/deepakvishwakarma-hh/momentary-talk#readme') }}>Read more
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="16"
+                                    height="16"
+                                    fill="currentColor"
+                                    viewBox="0 0 16 16">
+                                    <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+                                </svg></Text>
+                        </Flex>
+
+                        <Button
+                            letterSpacing={1}
+                            fontSize={20}
+                            variant="outline"
+                            color="white"
+                            fontFamily={"Fredoka One"}
+                            width={['30', 200]}
+                            tabIndex={1}
+                            onClick={onOpen}
+                            fontWeight={100}
+                            _hover={{ opacity: .8 }}
+                            _focus={{ opacity: .8 }}>
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
-                                width="16"
-                                height="16"
+                                width="20"
+                                height="20"
                                 fill="currentColor"
+                                className="bi bi-box"
                                 viewBox="0 0 16 16">
-                                <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
-                            </svg></Text>
-                    </Flex>
-
-                    <Button
-                        letterSpacing={1}
-                        fontSize={20}
-                        variant="outline"
-                        color="white"
-                        fontFamily={"Fredoka One"}
-                        width={['30', 200]}
-                        tabIndex={1}
-                        onClick={onOpen}
-                        fontWeight={100}
-                        _hover={{ opacity: .8 }}
-                        _focus={{ opacity: .8 }}>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="20"
-                            height="20"
-                            fill="currentColor"
-                            className="bi bi-box"
-                            viewBox="0 0 16 16">
-                            <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5 8 5.961 14.154 3.5 8.186 1.113zM15 4.239l-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923l6.5 2.6zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464L7.443.184z" /> <br />
-                        </svg>
-                    </Button>
+                                <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5 8 5.961 14.154 3.5 8.186 1.113zM15 4.239l-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923l6.5 2.6zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464L7.443.184z" /> <br />
+                            </svg>
+                        </Button>
 
 
-                    <Modal onClose={onClose} isOpen={isOpen} isCentered>
-                        <ModalOverlay backdropFilter='blur(10px) ' />
-                        <ModalContent bg={'blackAlpha.800'}>
-                            <ModalHeader letterSpacing={2} color="white" textTransform={'uppercase'} fontSize={20} >Customization</ModalHeader>
-                            <ModalCloseButton color="grey" />
-                            <ModalBody>
+                        <Modal onClose={onClose} isOpen={isOpen} isCentered>
+                            <ModalOverlay backdropFilter='blur(10px) ' />
+                            <ModalContent bg={'blackAlpha.800'}>
+                                <ModalHeader letterSpacing={2} color="white" textTransform={'uppercase'} fontSize={20} >Customization</ModalHeader>
+                                <ModalCloseButton color="grey" />
+                                <ModalBody>
 
-                                <Flex my={2} color={'white'} flexDir={'column'}>
-                                    <Text fontSize={20} color="tomato">Join</Text>
+                                    <Flex my={2} color={'white'} flexDir={'column'}>
+                                        <Text fontSize={20} color="tomato">Join</Text>
+                                        <Flex border={'1px grey solid'} borderRadius={5}>
+                                            <Input onChange={onJoinTokenHandler} border={'none'} flex={3} placeholder="Enter key to join" />
+                                            <Button onClick={onJoinHandler} borderLeft={'1px grey solid'} borderRadius={0} variant={'black'} flex={1} >
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                                                    <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+                                                </svg>
+                                            </Button>
+                                        </Flex>
+                                    </Flex>
+
+                                    <Text mt={4} fontSize={20} color="tomato">Setup</Text>
+
+                                    {/* <Text opacity={.8} color="white">Select option to terminate your room automatically. otherwise 5 minute by default.</Text> */}
                                     <Flex border={'1px grey solid'} borderRadius={5}>
-                                        <Input onChange={onJoinTokenHandler} border={'none'} flex={3} placeholder="Enter key to join" />
-                                        <Button onClick={onJoinHandler} borderLeft={'1px grey solid'} borderRadius={0} variant={'black'} flex={1} >
-                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                                        <Select flex={3} borderRadius={0} _focus={{ color: 'black' }}
+                                            onChange={onChangeHandler} variant='filled' placeholder='termination'>
+                                            <option value='0'>5 min</option>
+                                            <option value='1'>1 Hours</option>
+                                            <option value='2'>2 Hours</option>
+                                            <option value='3'>3 Hours</option>
+                                        </Select>
+                                        <Button flex={1} borderRadius={0}
+                                            w={'100%'}
+                                            border="none"
+                                            _hover={{ opacity: .8, bg: "none" }}
+                                            isLoading={loader}
+                                            variant='outline'
+                                            fontWeight={100}
+                                            color="white"
+                                            letterSpacing={2}
+                                            onClick={createRoom}
+                                        ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                                                 <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
                                             </svg>
                                         </Button>
                                     </Flex>
-                                </Flex>
-
-                                <Text mt={4} fontSize={20} color="tomato">Setup</Text>
-
-                                {/* <Text opacity={.8} color="white">Select option to terminate your room automatically. otherwise 5 minute by default.</Text> */}
-                                <Flex border={'1px grey solid'} borderRadius={5}>
-                                    <Select flex={3} borderRadius={0} _focus={{ color: 'black' }}
-                                        onChange={onChangeHandler} variant='filled' placeholder='termination'>
-                                        <option value='0'>5 min</option>
-                                        <option value='1'>1 Hours</option>
-                                        <option value='2'>2 Hours</option>
-                                        <option value='3'>3 Hours</option>
-                                    </Select>
-                                    <Button flex={1} borderRadius={0}
-                                        w={'100%'}
-                                        border="none"
-                                        _hover={{ opacity: .8, bg: "none" }}
-                                        isLoading={loader}
-                                        variant='outline'
-                                        fontWeight={100}
-                                        color="white"
-                                        letterSpacing={2}
-                                        onClick={createRoom}
-                                    ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                                            <path fillRule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
-                                        </svg>
-                                    </Button>
-                                </Flex>
-                            </ModalBody>
-                            <ModalFooter>
-                            </ModalFooter>
-                        </ModalContent>
-                    </Modal>
-                </Flex >
-            </Grid>
-            {/* </Validator > */}
+                                </ModalBody>
+                                <ModalFooter>
+                                </ModalFooter>
+                            </ModalContent>
+                        </Modal>
+                    </Flex >
+                </Grid>
+            </Validator >
 
         </>
 
